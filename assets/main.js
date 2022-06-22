c86de0d9f51851c347bce4fa0a8895adb30050f3
Paul-Siameto <siametopaul2@gmail.com>
2022-06-22 22:15:18 +0300

    fist commit

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
 document.getElementById('clickme').onclick = function(){
     var name = document.getElementById('inputtext').value;
-    document.write('Hello ',name,', welcome to the bootcamp');
+    console.log('Hello ',name,', welcome to the bootcamp');
 }
