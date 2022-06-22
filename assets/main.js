30c40caa3ca6b8044c692180107c8f65918508de
Paul-Siameto <siametopaul2@gmail.com>
2022-06-22 15:46:53 +0300

    first commit

---
+++ assets/main.js
@@ -0,0 +1,4 @@
+document.getElementById('clickme').onclick = function(){
+    var name = document.getElementById('inputtext').value;
+    document.write('Hello ',name,', welcome to the bootcamp');
+}
